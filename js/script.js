5d6c68eca44da231099da5d5de6bae33b9f06861
Ceres <pavlenkodim@mail.ru>
2023-01-13 00:57:37 +0600

    console app DB FilmsMovie ver1.0

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 "use strict";
 
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?');
+const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        gender: [],
+        privat: false
+    };
 
+const lastFilmName1 = prompt('Один из недавно просмотренных фильмов?'),
+      lastFilmRaiting1 = prompt('На сколько оцените его?'),
+      lastFilmName2 = prompt('Один из недавно просмотренных фильмов?'),
+      lastFilmRaiting2 = prompt('На сколько оцените его?');
+
+personalMovieDB.movies[lastFilmName1] = lastFilmRaiting1;
+personalMovieDB.movies[lastFilmName2] = lastFilmRaiting2;
+
+
+console.log(personalMovieDB);

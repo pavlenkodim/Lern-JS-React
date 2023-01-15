ada995ba9ef53e1c13b20fbc06e289c926c23f22
Ceres <pavlenkodim@mail.ru>
2023-01-15 21:25:23 +0600

    Add function trim() & did tasks

--- script1.js
+++ script1.js
@@ -180,3 +180,71 @@ console.log(Math.round(num));
 const test = "12.2px";
 console.log(parseInt(test));
 console.log(parseFloat(test));
+
+
+function calculateVolumeAndArea(num) {
+    if (num > 0 && typeof(num) === 'number' && Number.isInteger(num)) {
+        let v = num * num * num;
+        let s = (num * num + num * num + num * num) * 2;
+        return `Объем куба: ${v}, площадь всей поверхности: ${s}`;
+    } else {
+        return "При вычислении произошла ошибка!";
+    }
+}
+console.log(calculateVolumeAndArea(2));
+
+function getCoupeNumber(numPlace) {
+    if (Number.isInteger(numPlace) && numPlace > 0 && numPlace <= 36) {
+        let numCoupe = numPlace / 4;
+        return Math.ceil(numCoupe);
+    } else if (Number.isInteger(numPlace) && numPlace === 0 || numPlace > 36) {
+        return "Таких мест в вагоне не существует";
+    } else {
+        return "Ошибка. Проверьте правильность введенного номера места";
+    }
+}
+console.log(getCoupeNumber(36));
+
+function getTimeFromMinutes(minutes) {
+    if (Number.isInteger(minutes) && minutes >= 0 && minutes <= 600) {
+        let hours = Math.floor(minutes / 60);
+        minutes = minutes % 60;
+        if (hours === 1) {
+            return `Это ${hours} час и ${minutes} минут`;
+        } else if (hours >= 2 && hours < 5) {
+            return `Это ${hours} часа и ${minutes} минут`;
+        } else {
+            return `Это ${hours} часов и ${minutes} минут`;            
+        }
+    } else {
+        return "Ошибка, проверьте данные";
+    }
+}
+console.log(getTimeFromMinutes(0));
+
+function findMaxNumber(a, b, c, d) {
+    if ( typeof(a) === 'number' && typeof(b) === 'number' && typeof(c) === 'number' && typeof(d) === 'number') {
+        return Math.max(a, b, c, d);
+    } else {
+        return 0;
+    }
+}
+console.log(findMaxNumber(1, 5, "6.6", 11));
+
+/*задача на числа Фибоначчи, создать функцию, которая будет принимать число и возвращать сроку с числами 
+количество которых равно колличеству первого числа */
+
+function fib(num) {
+    if (Number.isInteger(num) && num > 0) {
+        let counts = [0, 1];
+        for (let i = 0; i < num; i++) {
+            counts.push(counts[i] + counts[i + 1]);
+        }
+        counts.pop();
+        counts.pop();
+        return counts.join(" ");
+    } else {
+        return "";
+    }
+}
+console.log(fib(7));

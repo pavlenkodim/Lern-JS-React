c351a286e2ac2342f04f8337364e9e7d5e1693e6
Ceres <pavlenkodim@mail.ru>
2023-02-07 22:08:49 +0600

    Переделал всю папку с обучением & Перешел на 2 главу. Теперь все папки будут обозначать главы & Работа над проэктом Food

--- Основы JavaScript/TestDOM/script.js
+++ Основы JavaScript/TestDOM/script.js
@@ -1,14 +1,14 @@
 // console.log(document.body);
 // console.log(document.documentElement);
-// console.log(document.body.childNodes);
-// console.log(document.body.firstChild);
+// console.log(document.body.childNodes); Позволяет полукчить все ноды и узлы этого родителя body
+// console.log(document.body.firstChild); Позволяет получить первого ребянка внутри родителя body
 // console.log(document.body.firstElementChild);
-// console.log(document.body.lastChild);
+// console.log(document.body.lastChild); Позволяет получить последнего ребянка внутри родителя body
 
-// console.log(document.querySelector("#current").parentNode);
-// console.log(document.querySelector("#current").parentElement);
+// console.log(document.querySelector("#current").parentNode); Получает своего родителя
+// console.log(document.querySelector("#current").parentElement); Получает 
 
-// console.log(document.querySelector('[data-current="3"]').nextElementSibling);
+// console.log(document.querySelector('[data-current="3"]').nextElementSibling); Получает соседний элемент
 
 // for (let node of document.body.childNodes) {
 //     if (node.nodeName == "#text") {
